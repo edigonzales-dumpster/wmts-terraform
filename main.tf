1221a6f380d6751548b782d26b214a815ed4aaed
Stefan Ziegler <edi.gonzales@gmail.com>
2019-11-27 09:15:49 +0100

    test user_data

--- main.tf
+++ main.tf
@@ -7,4 +7,12 @@ resource "digitalocean_droplet" "wmts" {
     region = "fra1"
     size   = "s-1vcpu-1gb"
     ssh_keys = [25503420,24397269]
+	user_data = << EOF
+		#! /bin/bash
+                sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
 }
