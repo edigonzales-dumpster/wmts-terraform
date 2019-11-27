5eb8617d865e3eda8ad30c2a7a3c3bcb36d44771
Stefan Ziegler <edi.gonzales@gmail.com>
2019-11-27 08:42:00 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "digitalocean" {}
+
+resource "digitalocean_droplet" "wmts" {
+    name  = "wmts"
+    #image = "ubuntu-18-04-x64"
+    image = "docker-18-04"
+    region = "fra1"
+    size   = "s-1vcpu-1gb"
+    ssh_keys = [25503420,24397269]
+}
