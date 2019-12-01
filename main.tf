bf9ba234dd7c72f3ca8435a0d943c733d672db49
Stefan Ziegler <edi.gonzales@gmail.com>
2019-12-01 18:49:41 +0100

    more cpu

--- main.tf
+++ main.tf
@@ -8,7 +8,8 @@ resource "digitalocean_droplet" "wmts" {
     #image = "ubuntu-18-04-x64"
     image = "docker-18-04"
     region = "fra1"
-    size   = "s-1vcpu-1gb"
+    #size   = "s-1vcpu-1gb"
+    size   = "s-3vcpu-1gb"
     ssh_keys = [25503420,24397269]
 	user_data = "${file("./user-data.yml")}"
     monitoring = true
