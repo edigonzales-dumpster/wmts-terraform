d0fb10f2fbba5676a8424020c65ff5a10a34fa38
Stefan Ziegler <edi.gonzales@gmail.com>
2019-12-02 19:22:26 +0100

    wip

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # wmts-terraform
 
+## Digitalocean API
+Get list of ssh keys:
 ```
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml down
+curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/account/keys" > ssh.json
 ```
+
+Get list of images:
+```
+curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?type=distribution" > images.json
+```
+(How to get marketplace images?)
+
+Get list of sizes:
+```
+curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/sizes" 
+````
--- main.tf
+++ main.tf
@@ -1,24 +1,29 @@
 variable "environment" {
 }
 
+variable "floating_ip" {
+}
+
 provider "digitalocean" {}
 
 resource "digitalocean_droplet" "wmts" {
     name  = "wmts-${var.environment}"
-    #image = "ubuntu-18-04-x64"
     image = "docker-18-04"
     region = "fra1"
-    #size   = "s-1vcpu-1gb"
     size   = "s-3vcpu-1gb"
     ssh_keys = [25503420,24397269]
 	user_data = "${file("./user-data.yml")}"
     monitoring = true
 }
 
-resource "digitalocean_project" "playground" {
-  name        = "WMTS-${var.environment}"
-  description = "WMTS"
-  purpose     = "Web Application"
-  #environment = "Development"
-  resources   = [digitalocean_droplet.wmts.urn]
+resource "digitalocean_project" "wmts" {
+    name        = "WMTS-${var.environment}"
+    description = "WMTS"
+    purpose     = "Web Application"
+    resources   = [digitalocean_droplet.wmts.urn]
 }
+
+#resource "digitalocean_floating_ip_assignment" "wmts" {
+#    ip_address = "${var.floating_ip}"
+#    droplet_id = digitalocean_droplet.wmts.id
+#}
